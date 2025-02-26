da5df9a94623718d824db4fcd353cc2ddf19f00b
Clay G <buzzscoutbark@gmail.com>
2025-02-26 16:03:50 -0600

    draft 1 complete

--- cheetah_ball_control_teaspoon/cheetah_ball_control_teaspoon.ino
+++ cheetah_ball_control_teaspoon/cheetah_ball_control_teaspoon.ino
@@ -1,12 +1,10 @@
-// these should match the pins the Lateral and throttle are connected to.
-// Green cables & LED are for the throttle(CH1), Blue for the Lateral (CH2).
-// 5 and 6 are selected as input pins as they are capable of PWM input unlike some other digital pins on the Nano
-const int ThrottlePin = 10;
-const int LateralPin = 11;
+// these should match the pins the Lateral and throttle are connected to from the RC Reciever.
+const int LateralPin = 10;
+const int ThrottlePin = 11;
 
-// Row 1 is the front row, Row 2 is the back row
 
-const int ENA_L_Pin= 9;
+// these should match the output pins to the L293 H-Bridge Motor Driver.
+const int ENA_L_Pin = 9;
 const int IN1_Pin = 8;
 const int IN2_Pin = 7;
 const int ENA_R_Pin = 5;
@@ -14,12 +12,21 @@ const int IN3_Pin = 4;
 const int IN4_Pin = 3;
 
 // these variables will change depending on the width of the pulses sent by the receiver
-unsigned long ThrottleState;
-unsigned long LateralState;
+unsigned long ThrottlePulse;
+unsigned long LateralPulse;
 
-// these variables will change from 0-255 depending on the output brightness of the LEDs
+// these variables will change from 0-255 depending on the strength of the pulse
+int ThrottleMappedPulse;
+int LateralMappedPulse;
+
+// these variables will change depending on the forwards-backwards / left-right direction of the command
 int ThrottleInterpretedState;
 int LateralInterpretedState;
+
+
+// determine the total strength of the PWM signals
+int ThrottleMotivation = 0;
+int LateralMotivation = 0;
 
 // robot current state (see #define for value meaning)
 int RobotState;
@@ -28,6 +35,11 @@ int RobotState;
 int MotorLState = 0;
 int MotorRState = 0;
 
+
+// determine the output speed of the motors to be sent to the ENA_L and ENA_R pins
+int MotorLSpeed = 0;
+int MotorRSpeed = 0;
+
 const int MIN_PULSE = 900UL;
 const int MAX_PULSE = 2100UL;
 
@@ -35,8 +47,8 @@ const int MAX_PULSE = 2100UL;
 const int static_variable = 0;
 
 // these need to be figured out experimentally based on seeing what the range on these pulse widths is
-const int Throttle_Duration_Max = 2009;
-const int Throttle_Duration_Min = 1009;
+const int Throttle_Duration_Max = 2090;
+const int Throttle_Duration_Min = 1119;
 const int Lateral_Duration_Max = 1879;
 const int Lateral_Duration_Min = 879;
 const int pulse_period = 20;
@@ -56,18 +68,11 @@ void setup() {
   Serial.begin(9600);
 }
 
-#define ROBOT_STATE_STATIONARY 0
-#define ROBOT_STATE_TURNING_LEFT 1 
-#define ROBOT_STATE_TURNING_RIGHT 2
-#define ROBOT_STATE_MOVING_FORWARDS 3
-#define ROBOT_STATE_MOVING_BACKWARDS 4
-
-#define MOTOR_STATE_BACKWARDS -1
-#define MOTOR_STATE_FORWARDS 1
-#define MOTOR_STATE_STATIONARY 0
-
-#define MOTOR_POWER_OFF 0
-#define MOTOR_POWER_ON 1
+#define STATIONARY 0
+#define LEFT 1
+#define RIGHT 2
+#define FORWARDS 3
+#define BACKWARDS 4
 
 byte GetPWM(byte pin)
 {
@@ -85,79 +90,111 @@ void loop() {
 
   // put your main code here, to run repeatedly:
   // LateralState and ThrottleState are the pulse widths measured from the reciever signal pins
-  unsigned long newLateralState = pulseIn(LateralPin, HIGH);
-  unsigned long newThrottleState = pulseIn(ThrottlePin, HIGH);
+  unsigned long newLateralPulse = pulseIn(LateralPin, HIGH);
+  unsigned long newThrottlePulse = pulseIn(ThrottlePin, HIGH);
 
-  if (newLateralState < 2100 && newLateralState > 600)
-    LateralState = newLateralState;
-  if (newThrottleState < 2100 && newThrottleState > 600)
-    ThrottleState = newThrottleState;
-  
+  if (newLateralPulse < 2100 && newLateralPulse > 600)
+    LateralPulse = newLateralPulse;
+  if (newThrottlePulse < 2100 && newThrottlePulse > 600)
+    ThrottlePulse = newThrottlePulse;
 
   // map those pulse widths to a scale from 0-255 to determine the LED brightness
-  LateralInterpretedState = map(LateralState, Lateral_Duration_Min, Lateral_Duration_Max, 0, 255);
-  ThrottleInterpretedState = map(ThrottleState, Throttle_Duration_Min, Throttle_Duration_Max, 0, 255);
+  LateralMappedPulse = map(LateralPulse, Lateral_Duration_Min, Lateral_Duration_Max, 0, 255);
+  ThrottleMappedPulse = map(ThrottlePulse, Throttle_Duration_Min, Throttle_Duration_Max, 0, 255);
 
-  // Determine Robot State
+  // turn it into more of an absolute magnitude from 1-127 in either direction  
+  // weird shit happens if LateralMappedPulse = 127 but thats ok because it triggers (Motivation < 20) in the next step and gets labeled stationary
+  LateralMotivation = (abs(LateralMappedPulse - 127) * 2) - 1;
+  ThrottleMotivation = (abs(ThrottleMappedPulse - 127) * 2) - 1;
 
-  // Determine Motor State
+  // determine direction of lateral and throttle based on whether the mapped pulse is big, and if so whether its greater than or less than 127
+  // there is a 'dead zone' here of 20 motivation in either direction that lets the robot stay stationary when the joystick is basically near the center. 
+  // we may want to adjust this later depending on how responsive this feels.-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  if (LateralMotivation < 20){
+    LateralInterpretedState = STATIONARY;
+  } else {
+    if ((LateralMappedPulse > 127) {
+      LateralInterpretedState = FORWARDS;
+    }
+    if ((LateralMappedPulse < 127) {
+      LateralInterpretedState = BACKWARDS;
+    }
+  };
 
+  if (ThrottleMotivation < 20){
+    ThrottleInterpretedState = STATIONARY;
+  } else {
+    if ((ThrottleMappedPulse > 127) {
+      ThrottleInterpretedState = FORWARDS;
+    }
+    if ((ThrottleMappedPulse < 127) {
+      ThrottleInterpretedState = BACKWARDS;
+    }
+  };
+  
 
-  // translate motor power state into relay command
+  // Determine Robot State -- decide whether 'turning' or 'forwards/backwards' wins
 
-  // translate motor state values into actual relay commands
-  // these are individual commands instead of one generalized function because 
-  // the wiring is difficult to adjust once installed and it is more convenient to 
-  // simply flip the code that runs that motor to operate inversely than it is to
-  // actually go dig around and fix things inside the electronics box
+  if (ThrottleMotivation > LateralMotivation) {
+    RobotState = ThrottleInterpretedState;
+  } else if (LateralMotivation > ThrottleMotivation) { 
+    RobotState = LateralInterpretedState;
+  } else {
+    RobotState = STATIONARY; 
+  }
 
-  // its a lazy solution but it is a solution that will work for the next few days of testing :)
+  // Determine Motor State based on Robot State
+  if (RobotState == FORWARDS) {
+    // do motor forwards stuff with the hbridge
+    analogWrite(ENA_L_Pin, ThrottleMotivation);
+    analogWrite(ENA_R_Pin, ThrottleMotivation);
+    digitalWrite(IN1_Pin, HIGH);
+    digitalWrite(IN2_Pin, LOW);
+    digitalWrite(IN3_Pin, HIGH);
+    digitalWrite(IN4_Pin, LOW);
+  } else if (RobotState == BACKWARDS) {
+    // do motor backwards stuff with the hbridge
+    analogWrite(ENA_L_Pin, ThrottleMotivation);
+    analogWrite(ENA_R_Pin, ThrottleMotivation);
+    digitalWrite(IN1_Pin, LOW);
+    digitalWrite(IN2_Pin, HIGH);
+    digitalWrite(IN3_Pin, LOW);
+    digitalWrite(IN4_Pin, HIGH);
+  } else if (RobotState == LEFT) {
+    // do motor left stuff with the hbridge
+    analogWrite(ENA_L_Pin, LateralMotivation);
+    analogWrite(ENA_R_Pin, LateralMotivation);
+    digitalWrite(IN1_Pin, HIGH);
+    digitalWrite(IN2_Pin, LOW);
+    digitalWrite(IN3_Pin, LOW);
+    digitalWrite(IN4_Pin, HIGH);
+  } else if (RobotState == RIGHT) {;
+    // do motor right stuff with the hbridge
+    analogWrite(ENA_L_Pin, LateralMotivation);
+    analogWrite(ENA_R_Pin, LateralMotivation);
+    digitalWrite(IN1_Pin, LOW);
+    digitalWrite(IN2_Pin, HIGH);
+    digitalWrite(IN3_Pin, HIGH);
+    digitalWrite(IN4_Pin, LOW);
+  } else {
+    // do motor stationary stuff
+    digitalWrite(IN1_Pin, LOW);
+    digitalWrite(IN2_Pin, LOW);
+    digitalWrite(IN3_Pin, LOW);
+    digitalWrite(IN4_Pin, LOW);
+  }
 
-  // big serial.print step for debugging and graphing
-  // Serial.print("ThrottlePin:");
-  // Serial.print(analogRead(ThrottlePin));
-  // Serial.print(",");
-  // Serial.print("LateralPin:");
-  // Serial.print(analogRead(LateralPin));
-  // Serial.print("Robot State:");
-  // Serial.print(RobotState);
-  // Serial.print(",");
-  // Serial.print("Left Motor State:");
-  // Serial.print(MotorL1State);
-  // Serial.print("Right Motor State:");
-  // Serial.print(MotorR1State);
-  // Serial.print("Left Motor Forward Pin:");
-  // Serial.print(digitalRead(MotorL1PinF));
-  // Serial.print("Left Motor Backward Pin:");
-  // Serial.print(digitalRead(MotorL1PinR));
-  // Serial.print("Right Motor Forward Pin:");
-  // Serial.print(digitalRead(MotorR1PinF));
-  // Serial.print("Right Motor Backward Pin:");
-  // Serial.print(digitalRead(MotorR1PinR));
-
-  //Serial.print("ThrottleInterpretedState:");
-  //Serial.print(ThrottleInterpretedState);
-  //Serial.print(",");
-  //Serial.print("LateralInterpretedState:");
-  //Serial.print(LateralInterpretedState);
-  //Serial.print(",");
-  // Serial.print("Static");
-  // Serial.print(static_variable);
-
-  // Serial.print("ThrottleInterpretedState:");
-  // Serial.print(GetPWM(ThrottlePin));
-  // Serial.print(",");
-  // Serial.print("LateralInterpretedState:");
-  // Serial.print(GetPWM(LateralPin));
-  Serial.print("lateralstate:");
-  Serial.print(LateralInterpretedState);
+  // serial.print step for debugging and graphing
+  Serial.print("lateral_motivation:");
+  Serial.print(LateralMotivation);
   Serial.print(",");
-  Serial.print("throttlestate:");
-  Serial.print(ThrottleInterpretedState);
+  Serial.print("throttle_motivation:");
+  Serial.print(ThrottleMotivation);
+  Serial.print(",");
+  Serial.print("robot_state:");
+  Serial.print(RobotState);
   Serial.print(",");
 
   Serial.println("");
   
-  //delay a bit
-  // delay(11);
 }
